917b69d9f4ef650145dbb12ac9f63d243e591c59
1hyzh <mr.ko12@icloud.com>
2025-04-07 16:15:53 +0200

    upload code

---
+++ Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim CustomizableEdges5 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges6 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges3 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges4 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(components)
+        Guna2DragControl1 = New Guna.UI2.WinForms.Guna2DragControl(components)
+        PictureBox1 = New PictureBox()
+        Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
+        Guna2Button2 = New Guna.UI2.WinForms.Guna2Button()
+        Guna2CheckBox1 = New Guna.UI2.WinForms.Guna2CheckBox()
+        Guna2Elipse2 = New Guna.UI2.WinForms.Guna2Elipse(components)
+        Guna2TextBox1 = New Guna.UI2.WinForms.Guna2TextBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Guna2Elipse1
+        ' 
+        Guna2Elipse1.BorderRadius = 10
+        Guna2Elipse1.TargetControl = Me
+        ' 
+        ' Guna2DragControl1
+        ' 
+        Guna2DragControl1.DockIndicatorTransparencyValue = 0.6R
+        Guna2DragControl1.TargetControl = Me
+        Guna2DragControl1.UseTransparentDrag = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.code
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(50, 50)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Guna2Button1
+        ' 
+        Guna2Button1.Animated = True
+        Guna2Button1.BackgroundImage = My.Resources.Resources.close
+        Guna2Button1.BorderRadius = 7
+        Guna2Button1.CustomizableEdges = CustomizableEdges5
+        Guna2Button1.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button1.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button1.FillColor = Color.FromArgb(CByte(141), CByte(157), CByte(182))
+        Guna2Button1.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        Guna2Button1.ForeColor = Color.White
+        Guna2Button1.Image = My.Resources.Resources.close
+        Guna2Button1.Location = New Point(335, 12)
+        Guna2Button1.Name = "Guna2Button1"
+        Guna2Button1.ShadowDecoration.CustomizableEdges = CustomizableEdges6
+        Guna2Button1.Size = New Size(50, 50)
+        Guna2Button1.TabIndex = 1
+        ' 
+        ' Guna2Button2
+        ' 
+        Guna2Button2.Animated = True
+        Guna2Button2.BorderRadius = 7
+        Guna2Button2.CustomizableEdges = CustomizableEdges3
+        Guna2Button2.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button2.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button2.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button2.FillColor = Color.FromArgb(CByte(141), CByte(157), CByte(182))
+        Guna2Button2.Font = New Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Guna2Button2.ForeColor = Color.White
+        Guna2Button2.Location = New Point(109, 94)
+        Guna2Button2.Name = "Guna2Button2"
+        Guna2Button2.ShadowDecoration.CustomizableEdges = CustomizableEdges4
+        Guna2Button2.Size = New Size(180, 45)
+        Guna2Button2.TabIndex = 2
+        Guna2Button2.Text = "Install"
+        ' 
+        ' Guna2CheckBox1
+        ' 
+        Guna2CheckBox1.Animated = True
+        Guna2CheckBox1.AutoSize = True
+        Guna2CheckBox1.BackColor = Color.FromArgb(CByte(141), CByte(157), CByte(182))
+        Guna2CheckBox1.BackgroundImageLayout = ImageLayout.None
+        Guna2CheckBox1.Checked = True
+        Guna2CheckBox1.CheckedState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        Guna2CheckBox1.CheckedState.BorderRadius = 0
+        Guna2CheckBox1.CheckedState.BorderThickness = 0
+        Guna2CheckBox1.CheckedState.FillColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        Guna2CheckBox1.CheckState = CheckState.Checked
+        Guna2CheckBox1.Location = New Point(138, 68)
+        Guna2CheckBox1.Name = "Guna2CheckBox1"
+        Guna2CheckBox1.Size = New Size(129, 19)
+        Guna2CheckBox1.TabIndex = 3
+        Guna2CheckBox1.Text = "Open after installed"
+        Guna2CheckBox1.UncheckedState.BorderColor = Color.FromArgb(CByte(125), CByte(137), CByte(149))
+        Guna2CheckBox1.UncheckedState.BorderRadius = 0
+        Guna2CheckBox1.UncheckedState.BorderThickness = 0
+        Guna2CheckBox1.UncheckedState.FillColor = Color.FromArgb(CByte(125), CByte(137), CByte(149))
+        Guna2CheckBox1.UseVisualStyleBackColor = False
+        ' 
+        ' Guna2Elipse2
+        ' 
+        Guna2Elipse2.BorderRadius = 2
+        Guna2Elipse2.TargetControl = Guna2CheckBox1
+        ' 
+        ' Guna2TextBox1
+        ' 
+        Guna2TextBox1.BorderColor = Color.FromArgb(CByte(188), CByte(202), CByte(214))
+        Guna2TextBox1.BorderRadius = 7
+        Guna2TextBox1.CustomizableEdges = CustomizableEdges1
+        Guna2TextBox1.DefaultText = "%appdata%\INTLS Injector"
+        Guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(CByte(208), CByte(208), CByte(208))
+        Guna2TextBox1.DisabledState.FillColor = Color.FromArgb(CByte(226), CByte(226), CByte(226))
+        Guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        Guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        Guna2TextBox1.FillColor = Color.FromArgb(CByte(188), CByte(202), CByte(214))
+        Guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        Guna2TextBox1.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        Guna2TextBox1.HoverState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        Guna2TextBox1.Location = New Point(85, 26)
+        Guna2TextBox1.Name = "Guna2TextBox1"
+        Guna2TextBox1.PlaceholderText = ""
+        Guna2TextBox1.SelectedText = ""
+        Guna2TextBox1.ShadowDecoration.CustomizableEdges = CustomizableEdges2
+        Guna2TextBox1.Size = New Size(235, 36)
+        Guna2TextBox1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(102), CByte(114), CByte(146))
+        ClientSize = New Size(397, 151)
+        Controls.Add(Guna2TextBox1)
+        Controls.Add(Guna2CheckBox1)
+        Controls.Add(Guna2Button2)
+        Controls.Add(Guna2Button1)
+        Controls.Add(PictureBox1)
+        FormBorderStyle = FormBorderStyle.None
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "INTLS"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Guna2DragControl1 As Guna.UI2.WinForms.Guna2DragControl
+    Friend WithEvents Guna2Button2 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2CheckBox1 As Guna.UI2.WinForms.Guna2CheckBox
+    Friend WithEvents Guna2Elipse2 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Guna2TextBox1 As Guna.UI2.WinForms.Guna2TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Net.Http
+Imports Newtonsoft.Json
+Imports System.IO
+
+Public Class Form1
+    ' Download the files based on JSON when Guna2Button2 is clicked
+    Private Async Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+        ' Check if path is empty
+        If Guna2TextBox1.Text = "" Then
+            MessageBox.Show("You must indicate the install path!", "INTLS", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Define the URL for the JSON that contains download URLs
+        Dim jsonUrl As String = "https://raw.githubusercontent.com/1hyzh/intls_ver/refs/heads/main/version.json" ' Replace with the actual URL to your JSON
+
+        ' Fetch and parse the JSON data
+        Dim client As New HttpClient()
+        Try
+            ' Get the JSON string from the remote URL
+            Dim jsonString As String = Await client.GetStringAsync(jsonUrl)
+
+            ' Parse the JSON string into a dynamic object
+            Dim jsonObject As Object = JsonConvert.DeserializeObject(jsonString)
+
+            ' Extract the URLs from the JSON
+
+            Dim injectorEXEurl As String = jsonObject("injectorEXEurl")
+            Dim zlibURL As String = jsonObject("zlibURL")
+            Dim libcurlURL As String = jsonObject("libcurlURL")
+
+            ' Define the destination folder path
+            Dim destinationFolder As String = Guna2TextBox1.Text
+
+            ' Check if the folder exists, if not, create it
+            If Not Directory.Exists(destinationFolder) Then
+                Directory.CreateDirectory(destinationFolder)
+            End If
+
+            ' Download the files
+
+            Await DownloadFileAsync(client, injectorEXEurl, destinationFolder)
+            Await DownloadFileAsync(client, zlibURL, destinationFolder)
+            Await DownloadFileAsync(client, libcurlURL, destinationFolder)
+
+            If Guna2CheckBox1.Checked = True Then
+                Process.Start(destinationFolder & "\_INTLS.exe")
+            End If
+        Catch ex As Exception
+            ' Handle any errors (e.g., JSON fetch errors, download errors)
+            MessageBox.Show("Error: " & ex.Message, "Download Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Helper function to download a file
+    Private Async Function DownloadFileAsync(client As HttpClient, fileUrl As String, destinationFolder As String) As Task
+        Try
+            ' Get the file name from the URL (e.g., file1.dll)
+            Dim fileName As String = Path.GetFileName(fileUrl)
+            Dim destinationPath As String = Path.Combine(destinationFolder, fileName)
+
+            ' Download the file asynchronously
+            Dim fileBytes As Byte() = Await client.GetByteArrayAsync(fileUrl)
+
+            ' Save the file to the specified path
+            Await File.WriteAllBytesAsync(destinationPath, fileBytes)
+
+
+        Catch ex As Exception
+            ' Handle errors specific to file download
+            MessageBox.Show("Error downloading " & Path.GetFileName(fileUrl) & ": " & ex.Message, "Download Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Function
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        End
+    End Sub
+End Class
